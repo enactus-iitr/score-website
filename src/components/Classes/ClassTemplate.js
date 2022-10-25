c6391671a824db3bdcbd2b42200b4259abd802fa
Daksh Mamodiya <dakshmamodiya138@gmail.com>
2022-10-25 12:17:12 +0530

    homepage property changed for netlify

--- src/components/Classes/ClassTemplate.js
+++ src/components/Classes/ClassTemplate.js
@@ -16,8 +16,6 @@ const ClassTemplate = ({ cls }) => {
         <h1 className={classes.heading}>{cls}th Class</h1>
         <p className={classes.classDesc}>
         {cls==11 && <p>Starting preparation for prestigious competitive exams like JEE Mains and JEE Advanced from class 11 itself gives the student a much-needed firm grasp over the basics and can put the student in a better position to crack these exams. In Class 11, Students are introduced to Mechanics, Physical Chemistry, and Algebra which provide the foundation for their preparation.</p>}
-        
-
         {cls==12 && <p>A student in their 12th class is given an opportunity to appear for one of the most prestigious engineering entrance exams in India. In order to be better placed to face this exam, prior preparation and strong basics in the concepts of class 12 is a must. In Class 12, Important Topics like Electrodynamics, Calculus, and Organic Chemistry are covered which are crucial for any exam.</p>}
         </p>
         <div className={classes.subjects}>
